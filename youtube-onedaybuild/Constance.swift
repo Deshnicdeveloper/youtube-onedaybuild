f8c26586ca2af78624bb6b1c9924b798dc12ce44
Nkwenti Deshnic <nkwentideshnic@gmail.com>
2020-08-15 13:05:11 +0100

    new file added

--- youtube-onedaybuild/Constance.swift
+++ youtube-onedaybuild/Constance.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Constance {
-    static var API_KEY = ""
+    static var API_KEY = "AIzaSyDueyu-6aQowNaylWU6ZXD3vSd1RL0TDwk"
     static var PLAYLIST_ID = "UUvG_RH3tAekGN5rPUVUmcIg"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constance.PLAYLIST_ID)&key=\(Constance.API_KEY)"
 }
---
+++ youtube-onedaybuild/ConstantTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by Nkwenti Deshnic on 15/08/2020.
+//  Copyright © 2020 Nkwenti Deshnic. All rights reserved.
+//
+
+/* Add your own keys below and uncomment the code*/
+
+import Foundation
+
+//struct Constance {
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constance.PLAYLIST_ID)&key=\(Constance.API_KEY)"
+//}
